11712dd913a42b2f26f8cbdcb54e3f1693ba2a5e
Sara Usevska <sarausevska42@yahoo.com>
2023-12-25 23:13:04 +0100

    first commit

---
+++ src/Broj1.java
@@ -0,0 +1,16 @@
+
+public class Broj1 {
+		  private int number1;
+		    private int number2;
+
+		    // Конструктор за поставување на вредности на броевите
+		    public Broj1(int num1, int num2) {
+		        this.number1 = num1;
+		        this.number2 = num2;
+		    }
+
+		    // Метод за собирање на броевите
+		    public int addNumbers() {
+		        return number1 + number2;
+		    }
+	}
---
+++ src/Broj2.java
@@ -0,0 +1,15 @@
+
+	 //Главна класа Broj2
+		public class Broj2 {
+		   public static void main(String[] args) {
+				  // Дефинирање на два броја
+		        int firstNumber = 7;
+		        int secondNumber = 23;
+
+		        // Креирање инстанца од класата Broj2
+		        Broj2  = new Broj2(firstNumber, secondNumber);
+
+		        // Печатење на резултатот од собирањето
+		        System.out.println("Сумата на броевите е: " + Broj2.addNumbers());
+		   }
+		}
